de58147adae62715dcfb8ae2224afaee7fbb7b5b
sukima <sukima945@gmail.com>
2023-06-20 22:57:24 +0900

    save

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm -rf ./build
+sudo rm -rf ./build
 
-rm -rf ./environment/portaudio_build
-rm -rf ./environment/soundtouch_build
+# sudo chmod -R 777 ./environment/soundtouch_build
+sudo rm -rf ./environment/portaudio_build
+sudo rm -rf ./environment/soundtouch_build
